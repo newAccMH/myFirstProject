39f0fe3b54b85876dd7e58a0e56c82a717a2db74
Michelle Hynds <michelle.hynds@ericsson.com>
2024-03-01 14:52:56 +0000

    Flask with no database

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+list_of_items = []
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        item = request.form['item']
+        list_of_items.append(item)
+        return render_template('index.html', items=list_of_items)
+    return render_template('index.html', items=list_of_items)
+
+
+@app.route('/delete/<item>', methods=['POST'])
+def delete_item(item):
+    if item in list_of_items:
+        list_of_items.remove(item)
+    return redirect(url_for('index'))
+
+
+@app.route('/update/<item>', methods=['POST'])
+def update_item(item):
+    if request.method == 'POST':
+        new_item = request.form['new_item']
+        if item in list_of_items:
+            item_index = list_of_items.index(item)
+            list_of_items[item_index] = new_item
+        return redirect(url_for('index'))
